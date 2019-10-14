99b165f1f8d25595d81fc289fe4711a0d02b4621
Sean Halpin <sean.e.halpin@gmail.com>
2019-10-14 19:46:38 +0100

    feat: adds consideration for vector nodes

--- code.ts
+++ code.ts
@@ -6,7 +6,9 @@ setTimeout(function(){
   const radius = 4
   const frame = figma.createFrame()
 
-  // const nodeInstance = figma.currentPage.findAll(f => f.type === 'INSTANCE');
+  const pink1 = 1
+  const pink2 = 0.3
+  const pink3 = 0.5
 
   frame.name = "Wire Box"
   frame.clipsContent = false
@@ -16,7 +18,7 @@ setTimeout(function(){
     if (node.type === "INSTANCE" ) {
 
       const rect = figma.createRectangle()
-      rect.name = node.id
+      rect.name = node.name
 
       const height = node.height
       const width = node.width
@@ -27,7 +29,7 @@ setTimeout(function(){
       rect.y = node.y
       rect.resize(node.width,node.height)
       rect.fills = [{type: 'SOLID', color: {r: 1, g: 1, b: 1}}]
-      rect.strokes = [{type: 'SOLID', color: {r: 1, g: 0.3, b: 0.5}}]
+      rect.strokes = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
       rect.topRightRadius = radius
       rect.topLeftRadius = radius
       rect.bottomLeftRadius = radius
@@ -36,6 +38,22 @@ setTimeout(function(){
       
       
       node.children.forEach(child => {
+
+        if (child.type === 'VECTOR') {
+          const vect = figma.createVector()
+          const height = child.height
+          const width = child.width
+          let newParentX = parentX
+          let newParentY = parentY
+
+          vect.resize(width,height)
+          vect.vectorNetwork = child.vectorNetwork
+          vect.x = newParentX + child.x
+          vect.y = newParentX + child.y
+          vect.strokeWeight = 0
+          vect.fills = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
+          frame.appendChild(vect)
+        }
         
         if (child.type === 'INSTANCE') {
 
@@ -54,7 +72,7 @@ setTimeout(function(){
             rect.x = newParentX + child.x
             rect.y = newParentY + child.y
             rect.fills = [{type: 'SOLID', color: {r: 1, g: 1, b: 1}}]
-            rect.strokes = [{type: 'SOLID', color: {r: 1, g: 0.3, b: 0.5}}]
+            rect.strokes = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
             rect.topRightRadius = radius
             rect.topLeftRadius = radius
             rect.bottomLeftRadius = radius
@@ -79,7 +97,7 @@ setTimeout(function(){
           textBlock.x = newParentX + child.x
           textBlock.y = newParentY + child.y
           textBlock.characters = child.characters
-          textBlock.fills = [{type: 'SOLID', color: {r: 1, g: 0.3, b: 0.5}}]
+          textBlock.fills = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
           textBlock.fontSize = child.fontSize
           textBlock.paragraphSpacing = child.paragraphSpacing
           textBlock.textAlignHorizontal = child.textAlignHorizontal
