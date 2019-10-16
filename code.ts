355118159805d1b391511ba68a42651aab6ca195
Sean Halpin <sean.e.halpin@gmail.com>
2019-10-16 10:08:27 +0100

    refactor: tidys up code
    - adds functions
    - improves nesting

--- code.ts
+++ code.ts
@@ -7,261 +7,262 @@ setTimeout(function(){
   const frame = figma.createFrame()
 
   const pink1 = 1
-  const pink2 = 0.3
-  const pink3 = 0.5
+  const pink2 = 0.15
+  const pink3 = 0.8
 
   frame.name = "Wire Box"
   frame.clipsContent = false
 
-  nodes.forEach(node => {
+  function rectOutline(node,xPos,yPos) {
+    const rect = figma.createRectangle()
     
-    if (node.type === "INSTANCE" ) {
+    rect.name = node.name
+    rect.x = xPos + node.x
+    rect.y = yPos + node.y
+    rect.resize(node.width,node.height)
+    rect.fills = [{type: 'SOLID', color: {r: 1, g: 1, b: 1}}]
+    rect.strokes = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
+    rect.topRightRadius = radius
+    rect.topLeftRadius = radius
+    rect.bottomLeftRadius = radius
+    rect.bottomRightRadius = radius
+    
+    frame.appendChild(rect)
+  }
 
-      const height = node.height
-      const width = node.width
-      const parentX = node.x
-      const parentY = node.y
+  function vectorOutline(node,xPos,yPos) {
+    const vect = figma.createVector()
+    const height = node.height
+    const width = node.width
+
+    vect.resize(width,height)
+    vect.vectorNetwork = node.vectorNetwork
+    vect.x = xPos + node.x
+    vect.y = yPos + node.y
+    vect.strokeWeight = 1
+    vect.strokes = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
+    
+    frame.appendChild(vect)
+  }
+
+  function textOutline(node,xPos,yPos) {
+    const textBlock = figma.createText()
+    const height = node.height
+    const width = node.width
+    
+    textBlock.name = node.name
+    textBlock.resize(width,height)
+    textBlock.x = xPos + node.x
+    textBlock.y = yPos + node.y
+
+    textBlock.characters = node.characters
+    textBlock.fills = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
+    textBlock.fontSize = node.fontSize
+    textBlock.lineHeight = node.lineHeight
+    textBlock.paragraphSpacing = node.paragraphSpacing
+    textBlock.textAlignHorizontal = node.textAlignHorizontal
+    textBlock.textAlignVertical = node.textAlignVertical
+    textBlock.textCase = node.textCase
+    textBlock.textDecoration = node.textDecoration
+
+    frame.appendChild(textBlock)
+  }
+
+  function ellipseOutline(node,xPos,yPos) {
+    const ellip = figma.createEllipse()
+    const height = node.height
+    const width = node.width
+
+    ellip.resize(width,height)
+    
+    ellip.x = xPos + node.x
+    ellip.y = yPos + node.y
+    ellip.strokeWeight = 1
+    ellip.fills = [{type: 'SOLID', color: {r: 1, g: 1, b: 1}}]
+    ellip.strokes = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
+
+    frame.appendChild(ellip)
+  }
 
 
-      // if (node.backgrounds.length >= 1){
-      //   const rect = figma.createRectangle()
-      //   rect.name = node.name
-      //   rect.x = node.x
-      //   rect.y = node.y
-      //   rect.resize(width,height)
-      //   rect.fills = [{type: 'SOLID', color: {r: 1, g: 1, b: 1}}]
-      //   rect.strokes = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-      //   rect.topRightRadius = radius
-      //   rect.topLeftRadius = radius
-      //   rect.bottomLeftRadius = radius
-      //   rect.bottomRightRadius = radius
+  nodes.forEach(node => {
 
-      //   frame.appendChild(rect)
-      // }
+    let parentX = node.x
+    let parentY = node.y
+    let parentName = node.name
+    
+    if (node.type === 'INSTANCE') {
 
-      const rect = figma.createRectangle()
-      rect.name = node.name
-      rect.x = node.x
-      rect.y = node.y
-      rect.resize(width,height)
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 1, b: 1}}]
-      rect.strokes = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-      rect.topRightRadius = radius
-      rect.topLeftRadius = radius
-      rect.bottomLeftRadius = radius
-      rect.bottomRightRadius = radius
+      if (node.backgrounds.length >=1) {
+        rectOutline(node,0,0)
+      }
 
-      frame.appendChild(rect)
-
-      
-      
-      
+      // console.log(parentX,parentY,parentName)
+           
       node.children.forEach(child => {
 
-        let nodeX = parentX + child.x
-        let nodeY = parentY + child.y
+        let parentXNested = parentX + child.x
+        let parentYNested = parentY + child.y
+        let parentName = node.name
+        
+        if (child.type === 'VECTOR') {         
+          vectorOutline(child,parentX,parentY)
+        }
 
-        // console.log(nodeX,nodeY, child.name)
-
-        if (child.type === 'VECTOR') {
+        if (child.type === 'RECTANGLE') {
           
-          const vect = figma.createVector()
-          const height = child.height
-          const width = child.width
-
-          let newParentX = parentX
-          let newParentY = parentY
-
-          vect.resize(width,height)
-          vect.vectorNetwork = child.vectorNetwork
-          vect.x = newParentX + child.x
-          vect.y = newParentY + child.y
-          vect.strokeWeight = 0
-          vect.fills = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-
-          frame.appendChild(vect)
+          if (child.visible === true) {
+            rectOutline(child,parentX,parentY)
+          }
+   
         }
         
         if (child.type === 'INSTANCE') {
 
-          let newParentX = parentX
-          let newParentY = parentY
+          if (child.visible === true && child.backgrounds.length >=1) {
 
-          let nestedX = child.x
-          let nestedY = child.y
-
-          // console.log(child.x, child.y)
-
-          if (child.visible === true) {
-            
-            const rect = figma.createRectangle()
-            const height = child.height
-            const width = child.width
-
-            rect.name = child.name
-            rect.resize(width,height)
-            rect.x = newParentX + child.x
-            rect.y = newParentY + child.y
-            rect.fills = [{type: 'SOLID', color: {r: 1, g: 1, b: 1}}]
-            rect.strokes = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-            rect.topRightRadius = radius
-            rect.topLeftRadius = radius
-            rect.bottomLeftRadius = radius
-            rect.bottomRightRadius = radius
-
-            frame.appendChild(rect)
+            rectOutline(child,parentX,parentY)
           }
 
           if ( "children" in child) {
+
+            if (child.visible === true) {
             
-            child.children.forEach(childInner => {
-              if (childInner.type === 'BOOLEAN_OPERATION') {
+              child.children.forEach(childInner => {
+
+                let parentXNestedNested = parentXNested + childInner.x
+                let parentYNestedNested = parentYNested + childInner.y
+                
+                if (childInner.type === 'BOOLEAN_OPERATION') {
+
+                  if( "children" in childInner) {
+
+                    childInner.children.forEach(childInnerInner => {
                       
-                // console.log(childInner.x, childInner.y)
-
-                if( "children" in childInner) {
-
-                  childInner.children.forEach(childInnerInner => {
-                    
-                    if (childInnerInner.type === 'VECTOR') {
-                      const vect = figma.createVector()
-                      const height = childInnerInner.height
-                      const width = childInnerInner.width
-            
-                      vect.resize(width,height)
-                      vect.vectorNetwork = childInnerInner.vectorNetwork
-                      vect.x = nestedX + childInnerInner.x
-                      vect.y = nestedY + childInnerInner.y
-                      // console.log(childInnerInner.x, childInnerInner.y)
-                      vect.strokeWeight = 1
-                      // vect.fills = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-                      vect.strokes = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-
-                      frame.appendChild(vect)
-                    }
-                  })
+                      if (childInnerInner.type === 'VECTOR') {
+                        vectorOutline(childInnerInner,parentXNestedNested,parentYNestedNested)
+                      }
+                    })
+                  }
                 }
-              }
-            })
+
+                if (childInner.type === 'VECTOR') {
+                  if (childInner.visible === true) {
+                    vectorOutline(childInner,parentXNested,parentYNested)
+                  }
+                }
+
+                if (childInner.type === 'TEXT') {
+                  if (childInner.visible === true) {
+                    textOutline(childInner,parentXNested,parentYNested)
+                  }
+
+                  
+                }
+
+                if (childInner.type === 'ELLIPSE') {
+                  if (childInner.visible === true) {
+                    ellipseOutline(childInner,parentXNested,parentYNested)
+                  }
+                }
+
+                if (childInner.type === 'RECTANGLE') {
+                  if (childInner.visible === true) {
+                    rectOutline(childInner,parentXNested,parentYNested)
+                  }
+                }
+
+              })
+            }
           }
 
         }
 
         if (child.type === 'GROUP') {
 
-          let newParentX = parentX
-          let newParentY = parentY
-          
-          // console.log(newParentX,newParentY, child.name)
+          if(child.visible === true) {
 
-          
-          if( "children" in child) {
             
-            child.children.forEach(childInner => {
-
-              if (childInner.type === 'TEXT') {
-
-                if (childInner.visible === true) {
-                 
-                  const textBlock = figma.createText()
-                  const height = childInner.height
-                  const width = childInner.width
+            
+            if( "children" in child) {
+            
+              child.children.forEach(childInner => {
+  
+                let parentInnerX = parentX + childInner.x
+                let parentInnerY = parentY + childInner.y
+  
+  
+                if (childInner.type === 'TEXT') {
+  
+                  if (childInner.visible === true) {
+                   
+                    textOutline(childInner,parentX,parentY)
                   
-                  textBlock.name = childInner.name
-                  textBlock.resize(width,height)
-                  textBlock.x = newParentX + childInner.x
-                  textBlock.y = newParentY + childInner.y
-
-                  // console.log(textBlock.x,textBlock.y, textBlock.name)
-
-                  textBlock.characters = childInner.characters
-                  textBlock.fills = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-                  textBlock.fontSize = childInner.fontSize
-                  textBlock.lineHeight = childInner.lineHeight
-                  textBlock.paragraphSpacing = childInner.paragraphSpacing
-                  textBlock.textAlignHorizontal = childInner.textAlignHorizontal
-                  textBlock.textAlignVertical = childInner.textAlignVertical
-                  textBlock.textCase = childInner.textCase
-                  textBlock.textDecoration = childInner.textDecoration
-      
-                  frame.appendChild(textBlock)
+                  }
                 }
-              }
-
-
-              if (childInner.type === 'INSTANCE') {
-
-                let childInnerX = parentX + childInner.x
-                let childInnerY = parentY + childInner.y
-                
-                if( "children" in childInner) {
+  
+                if (childInner.type === 'ELLIPSE') {
+  
+                  if (childInner.visible === true) {
+                    ellipseOutline(childInner,parentX,parentY)
                   
-                  childInner.children.forEach(childInnerInner => {
+                  }
+                }
+  
+                if (childInner.type === 'INSTANCE') {
+  
+                  if (childInner.visible === true) {
+  
+                    if (childInner.backgrounds.length >=1) {
+                      rectOutline(childInner,parentX,parentY)
+                    }
+  
+                  }
+                  
+                  if( "children" in childInner) {
                     
-                    if (childInnerInner.type === 'VECTOR' ) {
-                      
-                      const vect = figma.createVector()
-                      const height = childInnerInner.height
-                      const width = childInnerInner.width
-            
-                      vect.resize(width,height)
-                      vect.vectorNetwork = childInnerInner.vectorNetwork
-                      vect.x = childInnerX + childInnerInner.x
-                      vect.y = childInnerY + childInnerInner.y
-                      vect.strokeWeight = 0
-                      vect.fills = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-
-                      frame.appendChild(vect)
-                    }
-
-                    if (childInnerInner.type === 'ELLIPSE' ) {
-                      
-                      const rect = figma.createEllipse()
-                      const height = childInnerInner.height
-                      const width = childInnerInner.width
-            
-                      rect.resize(width,height)
-                      
-                      rect.x = childInnerX + childInnerInner.x
-                      rect.y = childInnerY + childInnerInner.y
-                      rect.strokeWeight = 2
-                      rect.fills = [{type: 'SOLID', color: {r: 1, g: 1, b: 1}}]
-                      rect.strokes = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-
-                      frame.appendChild(rect)
-                    }
-
-                    if (childInnerInner.type === 'BOOLEAN_OPERATION') {
-                      
-                      // console.log(childInnerInner.name)
-
-                      if( "children" in childInnerInner) {
-
-                        childInnerInner.children.forEach(childInnerInnerInner => {
-                          
-                          if (childInnerInnerInner.type === 'VECTOR') {
-                            const vect = figma.createVector()
-                            const height = childInnerInnerInner.height
-                            const width = childInnerInnerInner.width
-                  
-                            vect.resize(width,height)
-                            vect.vectorNetwork = childInnerInnerInner.vectorNetwork
-                            vect.x = childInnerX + childInnerInnerInner.x
-                            vect.y = childInnerY + childInnerInnerInner.y
-                            vect.strokeWeight = 1
-                            // vect.fills = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-                            vect.strokes = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-
-      
-                            frame.appendChild(vect)
+                    childInner.children.forEach(childInnerInner => {
+  
+                      if (childInner.visible === true) {
+                        if (childInnerInner.type === 'RECTANGLE' ) {
+                          if (childInnerInner.visible === true) {
+                            rectOutline(childInnerInner,parentInnerX,parentInnerY)
+                            console.log(childInnerInner.name)
                           }
-                        })
-                      }
-                    }
-                  })
+                        }
+                        
+                        if (childInnerInner.type === 'VECTOR' ) {
+                          if (childInnerInner.visible === true) {
+                            vectorOutline(childInnerInner,parentInnerX,parentInnerY)
+                          }
+                        }
+    
+                        if (childInnerInner.type === 'ELLIPSE' ) {
+                          if (childInnerInner.visible === true) {
+                            ellipseOutline(childInnerInner,parentInnerX,parentInnerY)
+                          }
+                        }
+    
+                        if (childInnerInner.type === 'BOOLEAN_OPERATION') {
+                          
+                          if( "children" in childInnerInner) {
+    
+                            childInnerInner.children.forEach(childInnerInnerInner => {
+                              
+                              if (childInnerInnerInner.type === 'VECTOR') {
+                                if (childInnerInnerInner.visible === true) {
+                                  vectorOutline(childInnerInnerInner,parentInnerX,parentInnerY)
+                                }
+                              }
+                            })
+                          }
+                        }
+                      }                                  
+                    })
+                  }
                 }
-              }
-            });
+              });
+            }
           }
         }
 
@@ -269,29 +270,10 @@ setTimeout(function(){
 
           if (child.visible === true) {
            
-            const textBlock = figma.createText()
-            const height = child.height
-            const width = child.width
+            textOutline(child,parentX,parentY)
 
-            let parent = child.parent
-            let newParentX = parentX
-            let newParentY = parentY
-          
-            textBlock.name = child.name
-            textBlock.resize(width,height)
-            textBlock.x = newParentX + child.x
-            textBlock.y = newParentY + child.y
-            textBlock.characters = child.characters
-            textBlock.fills = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-            textBlock.fontSize = child.fontSize
-            textBlock.paragraphSpacing = child.paragraphSpacing
-            textBlock.lineHeight = child.lineHeight
-            textBlock.textAlignHorizontal = child.textAlignHorizontal
-            textBlock.textAlignVertical = child.textAlignVertical
-            textBlock.textCase = child.textCase
-            textBlock.textDecoration = child.textDecoration
-
-            frame.appendChild(textBlock)
+            // console.log(parentX,parentY,parentName)
+            // console.log(child.x,child.y,child.name)
           }
         }
 
