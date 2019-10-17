a6b29c5b068bfa2a997989670d5ce8c94259f28c
Sean Halpin <sean.e.halpin@gmail.com>
2019-10-17 19:14:49 +0100

    feat: adds support for polygons. updates frame size based on largest parent block

--- code.js
+++ code.js
@@ -84,33 +84,57 @@ setTimeout(function () {
         ellip.strokes = [{ type: 'SOLID', color: { r: pink1, g: pink2, b: pink3 } }];
         frame.appendChild(ellip);
     }
+    function polygoneOutline(node) {
+        const poly = figma.createPolygon();
+        const height = node.height;
+        const width = node.width;
+        let transformPos = node.absoluteTransform;
+        let newX = transformPos[0][2];
+        let newY = transformPos[1][2];
+        poly.rotation = node.rotation;
+        poly.resize(width, height);
+        poly.x = newX;
+        poly.y = newY;
+        poly.strokeWeight = 1;
+        poly.fills = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 } }];
+        poly.strokes = [{ type: 'SOLID', color: { r: pink1, g: pink2, b: pink3 } }];
+        frame.appendChild(poly);
+    }
     // determine parameters for showing
     function drawItems(node, nodeParent) {
         if (nodeParent.visible === true) {
             if (node.type === 'INSTANCE' || node.type === 'COMPONENT') {
                 if (node.visible === true) {
-                    if (node.backgrounds.length >= 1) {
+                    if (node.backgrounds.length >= 1 && node.width >= 0.1 && node.height >= 0.1) {
                         rectOutline(node);
                     }
                 }
             }
             if (node.type === 'VECTOR') {
-                if (node.visible === true) {
+                if (node.visible === true && node.width >= 0.1 && node.height >= 0.1) {
                     vectorOutline(node);
                 }
             }
+            if (node.type === 'POLYGON') {
+                if (node.visible === true) {
+                    polygoneOutline(node);
+                }
+            }
             if (node.type === 'RECTANGLE') {
-                if (node.visible === true && node.fills.length >= 1) {
+                if (node.visible === true && node.fills.length >= 1 && node.width >= 0.1 && node.height >= 0.1) {
+                    rectOutline(node);
+                }
+                if (node.strokeWeight >= 1.1) {
                     rectOutline(node);
                 }
             }
             if (node.type === 'ELLIPSE') {
-                if (node.visible === true) {
+                if (node.visible === true && node.width >= 0.1 && node.height >= 0.1) {
                     ellipseOutline(node);
                 }
             }
             if (node.type === 'TEXT') {
-                if (node.visible === true) {
+                if (node.visible === true && node.width >= 0.1 && node.height >= 0.1) {
                     textOutline(node);
                 }
             }
@@ -118,8 +142,12 @@ setTimeout(function () {
     }
     // execute
     function recurse(parents) {
+        let arrayWidth = [];
+        let arrayHeight = [];
         parents.forEach(parent => {
             drawItems(parent, parent);
+            arrayWidth.push(parent.width);
+            arrayHeight.push(parent.height);
             parent.children.forEach(child => {
                 drawItems(child, parent);
                 if ("children" in child) {
@@ -137,6 +165,11 @@ setTimeout(function () {
                                                 if ("children" in greatgreatgreatgranchild) {
                                                     greatgreatgreatgranchild.children.forEach(greatgreatgreatgreatgranchild => {
                                                         drawItems(greatgreatgreatgreatgranchild, greatgreatgreatgranchild);
+                                                        if ("children" in greatgreatgreatgreatgranchild) {
+                                                            greatgreatgreatgreatgranchild.children.forEach(greatgreatgreatgreatgreatgranchild => {
+                                                                drawItems(greatgreatgreatgreatgreatgranchild, greatgreatgreatgreatgranchild);
+                                                            });
+                                                        }
                                                     });
                                                 }
                                             });
@@ -149,6 +182,12 @@ setTimeout(function () {
                 }
             });
         });
+        console.log(arrayWidth);
+        let frameWidth = Math.max(...arrayWidth);
+        let frameheight = Math.max(...arrayHeight);
+        frame.resize(frameWidth, frameheight);
+        frame.x = frameWidth + 100;
+        frame.y = frameheight - frameheight;
     }
     recurse(nodes);
     figma.closePlugin();
