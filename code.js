22502e2714539090e82c31083fd4cf0678e23852
Sean Halpin <sean.e.halpin@gmail.com>
2019-10-18 11:48:06 +0100

    Refactor: Couple of fixes as textnodes get borked a lot - API returns mixed values. Adding another check to see if a nodes grandparent is visible. Reworked my absurd naming convention.

--- code.js
+++ code.js
@@ -8,8 +8,6 @@ setTimeout(function () {
     const radius = 4;
     frame.name = "Wire Box";
     frame.clipsContent = false;
-    let xPos = 0;
-    let yPos = 0;
     // define shape types
     function rectOutline(node) {
         const rect = figma.createRectangle();
@@ -59,13 +57,18 @@ setTimeout(function () {
         textBlock.y = newY;
         textBlock.characters = node.characters;
         textBlock.fills = [{ type: 'SOLID', color: { r: pink1, g: pink2, b: pink3 } }];
-        textBlock.fontSize = node.fontSize;
+        if (typeof node.fontSize == 'number') {
+            textBlock.fontSize = node.fontSize;
+        }
+        else {
+            textBlock.fontSize = 14;
+        }
         textBlock.lineHeight = node.lineHeight;
         textBlock.paragraphSpacing = node.paragraphSpacing;
         textBlock.textAlignHorizontal = node.textAlignHorizontal;
         textBlock.textAlignVertical = node.textAlignVertical;
         textBlock.textCase = node.textCase;
-        textBlock.textDecoration = node.textDecoration;
+        // textBlock.textDecoration = node.textDecoration
         frame.appendChild(textBlock);
     }
     function ellipseOutline(node) {
@@ -84,7 +87,7 @@ setTimeout(function () {
         ellip.strokes = [{ type: 'SOLID', color: { r: pink1, g: pink2, b: pink3 } }];
         frame.appendChild(ellip);
     }
-    function polygoneOutline(node) {
+    function polygonOutline(node) {
         const poly = figma.createPolygon();
         const height = node.height;
         const width = node.width;
@@ -101,8 +104,8 @@ setTimeout(function () {
         frame.appendChild(poly);
     }
     // determine parameters for showing
-    function drawItems(node, nodeParent) {
-        if (nodeParent.visible === true) {
+    function drawItems(node, nodeParent, nodeGrandParent) {
+        if (nodeParent.visible === true && nodeGrandParent.visible === true) {
             if (node.type === 'INSTANCE' || node.type === 'COMPONENT') {
                 if (node.visible === true) {
                     if (node.backgrounds.length >= 1 && node.width >= 0.1 && node.height >= 0.1) {
@@ -116,15 +119,18 @@ setTimeout(function () {
                 }
             }
             if (node.type === 'POLYGON') {
-                if (node.visible === true) {
-                    polygoneOutline(node);
+                if (node.visible === true && node.width >= 0.1 && node.height >= 0.1) {
+                    polygonOutline(node);
                 }
             }
             if (node.type === 'RECTANGLE') {
-                if (node.visible === true && node.fills.length >= 1 && node.width >= 0.1 && node.height >= 0.1) {
+                if (node.visible === true && node.fills.length >= 1 && node.width >= 0.1 && node.height >= 0.1 && node.isMask !== true) {
                     rectOutline(node);
                 }
-                if (node.strokeWeight >= 1.1) {
+                if (node.visible === true && node.strokeWeight >= 1.1) {
+                    rectOutline(node);
+                }
+                if (node.visible === true && node.strokeWeight === 1 && node.fills.length === 0) {
                     rectOutline(node);
                 }
             }
@@ -145,29 +151,39 @@ setTimeout(function () {
         let arrayWidth = [];
         let arrayHeight = [];
         parents.forEach(parent => {
-            drawItems(parent, parent);
+            drawItems(parent, parent, parent);
             arrayWidth.push(parent.width);
             arrayHeight.push(parent.height);
             parent.children.forEach(child => {
-                drawItems(child, parent);
+                drawItems(child, parent, parent);
                 if ("children" in child) {
                     child.children.forEach(granchild => {
-                        drawItems(granchild, child);
+                        drawItems(granchild, child, parent);
                         if ("children" in granchild) {
                             granchild.children.forEach(greatgranchild => {
-                                drawItems(greatgranchild, granchild);
+                                drawItems(greatgranchild, granchild, child);
                                 if ("children" in greatgranchild) {
                                     greatgranchild.children.forEach(greatgreatgranchild => {
-                                        drawItems(greatgreatgranchild, greatgranchild);
+                                        drawItems(greatgreatgranchild, greatgranchild, granchild);
                                         if ("children" in greatgreatgranchild) {
-                                            greatgreatgranchild.children.forEach(greatgreatgreatgranchild => {
-                                                drawItems(greatgreatgreatgranchild, greatgreatgranchild);
-                                                if ("children" in greatgreatgreatgranchild) {
-                                                    greatgreatgreatgranchild.children.forEach(greatgreatgreatgreatgranchild => {
-                                                        drawItems(greatgreatgreatgreatgranchild, greatgreatgreatgranchild);
-                                                        if ("children" in greatgreatgreatgreatgranchild) {
-                                                            greatgreatgreatgreatgranchild.children.forEach(greatgreatgreatgreatgreatgranchild => {
-                                                                drawItems(greatgreatgreatgreatgreatgranchild, greatgreatgreatgreatgranchild);
+                                            greatgreatgranchild.children.forEach(greatThreegranchild => {
+                                                drawItems(greatThreegranchild, greatgreatgranchild, greatgranchild);
+                                                if ("children" in greatThreegranchild) {
+                                                    greatThreegranchild.children.forEach(greatFourgranchild => {
+                                                        drawItems(greatFourgranchild, greatThreegranchild, greatgreatgranchild);
+                                                        if ("children" in greatFourgranchild) {
+                                                            greatFourgranchild.children.forEach(greatFivegranchild => {
+                                                                drawItems(greatFivegranchild, greatFourgranchild, greatThreegranchild);
+                                                                if ("children" in greatFivegranchild) {
+                                                                    greatFivegranchild.children.forEach(greatSixgranchild => {
+                                                                        drawItems(greatSixgranchild, greatFivegranchild, greatFourgranchild);
+                                                                        if ("children" in greatSixgranchild) {
+                                                                            greatSixgranchild.children.forEach(greatSevengranchild => {
+                                                                                drawItems(greatSevengranchild, greatSixgranchild, greatFivegranchild);
+                                                                            });
+                                                                        }
+                                                                    });
+                                                                }
                                                             });
                                                         }
                                                     });
@@ -182,7 +198,6 @@ setTimeout(function () {
                 }
             });
         });
-        console.log(arrayWidth);
         let frameWidth = Math.max(...arrayWidth);
         let frameheight = Math.max(...arrayHeight);
         frame.resize(frameWidth, frameheight);
--- code.ts
+++ code.ts
@@ -11,9 +11,6 @@ setTimeout(function(){
   frame.name = "Wire Box"
   frame.clipsContent = false
 
-  let xPos = 0
-  let yPos = 0
-
   // define shape types
 
   function rectOutline(node) {
@@ -75,13 +72,19 @@ setTimeout(function(){
 
     textBlock.characters = node.characters
     textBlock.fills = [{type: 'SOLID', color: {r: pink1, g: pink2, b: pink3}}]
-    textBlock.fontSize = node.fontSize
+
+    if (typeof node.fontSize == 'number') {
+      textBlock.fontSize = node.fontSize
+    } else {
+      textBlock.fontSize = 14
+    }
+    
     textBlock.lineHeight = node.lineHeight
     textBlock.paragraphSpacing = node.paragraphSpacing
     textBlock.textAlignHorizontal = node.textAlignHorizontal
     textBlock.textAlignVertical = node.textAlignVertical
     textBlock.textCase = node.textCase
-    textBlock.textDecoration = node.textDecoration
+    // textBlock.textDecoration = node.textDecoration
 
     frame.appendChild(textBlock)
   }
@@ -106,7 +109,7 @@ setTimeout(function(){
     frame.appendChild(ellip)
   }
 
-  function polygoneOutline(node) {
+  function polygonOutline(node) {
     const poly = figma.createPolygon()
     const height = node.height
     const width = node.width
@@ -128,9 +131,9 @@ setTimeout(function(){
 
   // determine parameters for showing
 
-  function drawItems(node,nodeParent) {
+  function drawItems(node,nodeParent,nodeGrandParent) {
 
-    if(nodeParent.visible === true) {
+    if(nodeParent.visible === true && nodeGrandParent.visible === true) {
       if (node.type === 'INSTANCE' || node.type ==='COMPONENT') {
         if (node.visible === true ) {
           if (node.backgrounds.length >=1 && node.width >= 0.1 && node.height >= 0.1) {
@@ -144,15 +147,19 @@ setTimeout(function(){
         }
       }
       if (node.type === 'POLYGON') {
-        if (node.visible === true) {       
-          polygoneOutline(node)
+        if (node.visible === true && node.width >= 0.1 && node.height >= 0.1) {       
+          polygonOutline(node)
         }
       }
       if (node.type === 'RECTANGLE') {      
-        if (node.visible === true && node.fills.length >=1 && node.width >= 0.1 && node.height >= 0.1) {
+        if (node.visible === true && node.fills.length >=1 && node.width >= 0.1 && node.height >= 0.1 && node.isMask !== true) {
           rectOutline(node)
         }
-        if (node.strokeWeight >= 1.1) {
+        if (node.visible === true && node.strokeWeight >= 1.1) {
+          rectOutline(node)
+        }
+
+        if (node.visible === true && node.strokeWeight === 1 && node.fills.length === 0) {
           rectOutline(node)
         }
       }
@@ -161,7 +168,7 @@ setTimeout(function(){
           ellipseOutline(node)
         }
       }
-      if (node.type === 'TEXT') {      
+      if (node.type === 'TEXT') {  
         if (node.visible === true && node.width >= 0.1 && node.height >= 0.1) {
           textOutline(node)
         }
@@ -177,38 +184,50 @@ setTimeout(function(){
     let arrayHeight = []
     
     parents.forEach(parent => {
-      drawItems(parent,parent)
+      drawItems(parent,parent,parent)
 
       arrayWidth.push(parent.width)
       arrayHeight.push(parent.height)
 
       parent.children.forEach(child => {
         
-        drawItems(child,parent)
+        drawItems(child,parent,parent)
 
         if ( "children" in child) {
           child.children.forEach(granchild => {
-            drawItems(granchild,child)
+            drawItems(granchild,child,parent)
 
             if ( "children" in granchild) {
               granchild.children.forEach(greatgranchild => {
-                drawItems(greatgranchild,granchild)
+                drawItems(greatgranchild,granchild,child)
 
                 if ( "children" in greatgranchild) {
                   greatgranchild.children.forEach(greatgreatgranchild => {
-                    drawItems(greatgreatgranchild,greatgranchild)
+                    drawItems(greatgreatgranchild,greatgranchild,granchild)
 
                     if ( "children" in greatgreatgranchild) {
-                      greatgreatgranchild.children.forEach(greatgreatgreatgranchild => {
-                        drawItems(greatgreatgreatgranchild,greatgreatgranchild)
+                      greatgreatgranchild.children.forEach(greatThreegranchild => {
+                        drawItems(greatThreegranchild,greatgreatgranchild,greatgranchild)
 
-                        if ( "children" in greatgreatgreatgranchild) {
-                          greatgreatgreatgranchild.children.forEach(greatgreatgreatgreatgranchild => {
-                            drawItems(greatgreatgreatgreatgranchild,greatgreatgreatgranchild)
+                        if ( "children" in greatThreegranchild) {
+                          greatThreegranchild.children.forEach(greatFourgranchild => {
+                            drawItems(greatFourgranchild,greatThreegranchild,greatgreatgranchild)
 
-                            if ("children" in greatgreatgreatgreatgranchild) {
-                              greatgreatgreatgreatgranchild.children.forEach(greatgreatgreatgreatgreatgranchild => {
-                                drawItems(greatgreatgreatgreatgreatgranchild,greatgreatgreatgreatgranchild)
+                            if ("children" in greatFourgranchild) {
+                              greatFourgranchild.children.forEach(greatFivegranchild => {
+                                drawItems(greatFivegranchild,greatFourgranchild,greatThreegranchild)
+
+                                if ("children" in greatFivegranchild) {
+                                  greatFivegranchild.children.forEach(greatSixgranchild => {
+                                    drawItems(greatSixgranchild,greatFivegranchild,greatFourgranchild)
+
+                                    if ("children" in greatSixgranchild) {
+                                      greatSixgranchild.children.forEach(greatSevengranchild => {
+                                        drawItems(greatSevengranchild,greatSixgranchild,greatFivegranchild)
+                                      })
+                                    }
+                                  })
+                                }
                               })
                             }
                           })
@@ -223,7 +242,6 @@ setTimeout(function(){
         }
       })
     })
-    console.log(arrayWidth)
 
     let frameWidth = Math.max(...arrayWidth)
     let frameheight = Math.max(...arrayHeight)
